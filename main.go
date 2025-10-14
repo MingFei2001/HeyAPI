fc72d40a6b7354ec37a03280fc70bc790738f8ef
mingfei <mingfei@themfthinkpad>
2025-10-14 20:57:56 +0800

    added some comments

--- main.go
+++ main.go
@@ -5,16 +5,20 @@ import (
 	"net/http"
 )
 
+// define port number used
 const port = ":8080"
 
+// function to serve the html page
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "templates/index.html")
 }
 
+// function to serve the API endpoint
 func APIHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hey the API works!")
 }
 
+// main function to start the server
 func main() {
 	http.HandleFunc("/", HomeHandler)
 	http.HandleFunc("/api", APIHandler)
