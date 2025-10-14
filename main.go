8535e1d19e3d5b8ea0647248a21a15d6c98912ef
mingfei <mingfei@themfthinkpad>
2025-10-14 20:34:07 +0800

    added code to serve the html page

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 	"net/http"
 )
 
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/index.html")
+}
+
 func APIHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hey the API works!")
 }
 
 func main() {
+	http.HandleFunc("/", HomeHandler)
 	http.HandleFunc("/api", APIHandler)
 
 	fmt.Println("Server is running at http://localhost:8080")
