822448ac7f7e51d357c256ee0d453913b761ece1
mingfei <mingfei@themfthinkpad>
2025-10-26 22:03:48 +0800

    added basic struct for api response and page data

--- handlers/currency.go
+++ handlers/currency.go
@@ -2,6 +2,17 @@ package handlers
 
 import "net/http"
 
+// define type for decoding JSON response from exchange rate API
+type CurrencyAPIResponse struct {
+	// placeholder
+	Amount   float64 `json:"amount"`
+	Currency string  `json:"currency"`
+}
+
+type CurrencyPageData struct {
+	Result float64
+}
+
 // main logic
 func CurrencyHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "templates/currency.html")
