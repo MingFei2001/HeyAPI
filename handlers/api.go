b27063900acb4c579ac75550792c4e4644b8f34e
mingfei <mingfei@themfthinkpad>
2025-12-15 19:49:27 +0800

    added a non GET method handler

--- handlers/api.go
+++ handlers/api.go
@@ -2,9 +2,19 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func APIHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Handles non GET requests, return 405
+	if r.Method != http.MethodGet {
+		log.Printf("Invalid method: %s", r.Method)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// If nothing goes wrong
 	fmt.Fprintf(w, "Hey the API works!")
 }
